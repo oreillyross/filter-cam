9c1e061947b404ab3a77bfb9cd5b374c1b7a80b1
Faktor 10 <oreillyross@gmail.com>
2022-01-23 16:38:25 +0100

    feature: add video stream

--- camera.js
+++ camera.js
@@ -1,1 +1,21 @@
-console.log('yes it works')
+(function() {
+
+  function startup() {
+      const video = document.getElementById("video");
+
+      navigator.mediaDevices.getUserMedia({video: true, audio: false})
+      .then(function(stream) {
+          video.srcObject = stream;
+          video.play();
+
+      })
+      .catch(function(error) {
+          console.log(`An error occured loading media: ${error}`)
+      })
+  }
+ 
+document.addEventListener("load", startup, false);
+
+})();
+
+
